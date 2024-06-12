a2de3f84cd56d34da152314c2f14577c35477e36
gitsanghyun <gks7654@gmail.com>
2024-06-13 00:13:58 +0900

    img

--- oeMarket/src/oe/market/common/CommonUtils.java
+++ oeMarket/src/oe/market/common/CommonUtils.java
@@ -6,8 +6,8 @@ public class CommonUtils {
 	public static final int IMG_FILE_SIZE = 10*1024*1024;
 	public static final String IMG_EN_CODE = "UTF-8";
 	
-	public static final int BOARD_PAGE_SIZE = 3;
-	public static final int BOARD_GROUP_SIZE = 3;
+	public static final int BOARD_PAGE_SIZE = 9;
+	public static final int BOARD_GROUP_SIZE = 9;
 	public static final int BOARD_CUR_PAGE = 1;
 	public static final int BOARD_TOTAL_COUNT = 0;
 	
